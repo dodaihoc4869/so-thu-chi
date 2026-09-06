3cd0ee68dbb306bfec04c157cd0dcdf0a4ca1d18
OBXJoJo <ttadodaihoc@gmail.com>
2026-09-06 01:55:01 +0000

    Icon mới: hoa sen phẳng trên nền hồng, phân biệt hai sổ bằng sắc độ

--- hh/sw.js
+++ hh/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "so-thu-chi-v1";
+const CACHE = "so-thu-chi-hh-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json",
                 "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
