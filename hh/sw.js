7222a51e1c62c9ff5446e89c199d976a0c73666c
OBXJoJo <ttadodaihoc@gmail.com>
2026-09-06 03:20:35 +0000

    Man Ghi: mot bang TONG CHI cong tu ba the quy, thay ba o Thu/Chi/Con lai
    - ghiTotalsHtml() cong dung tu bucketSums, khong tinh lai bang duong khac
    - Dong phu giu lai so thu va con lai, khong mat du lieu
    - Tab Thang van giu ba o Thu/Chi/Con lai nhu cu

--- hh/sw.js
+++ hh/sw.js
@@ -1,5 +1,5 @@
 const PREFIX = "so-thu-chi-hh-";
-const CACHE = PREFIX + "v8";
+const CACHE = PREFIX + "v9";
 const ASSETS = ["./", "./index.html", "./manifest.json",
                 "./icon-192.png?v=2", "./icon-512.png?v=2", "./icon-maskable-512.png?v=2"];
 
